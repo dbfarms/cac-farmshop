949291808dd75067efab3c7417c1869c93ca4bdc
Matt Hunger <matt.hunger@gmail.com>
2018-02-13 16:47:35 -0500

    safety commit

--- cac-farmshop-client/src/index.js
+++ cac-farmshop-client/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './containers/App';
+import Login from './containers/Login'
 import registerServiceWorker from './registerServiceWorker';
 import store from './store.js'
 
 ReactDOM.render(
   <Provider store={store} >
       <div>
+        <Login />
         <App />
       </div>
   </Provider>,
