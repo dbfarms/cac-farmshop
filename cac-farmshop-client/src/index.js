7e9bece316d66e255722cac2bda4a92ac0c315c1
Matt Hunger <matt.hunger@gmail.com>
2018-02-17 13:57:44 -0500

    back on track

--- cac-farmshop-client/src/index.js
+++ cac-farmshop-client/src/index.js
@@ -4,13 +4,10 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './containers/App';
-import { AppLogin } from './AppLogin/AppLogin';
 import Login from './containers/Login'
 import registerServiceWorker from './registerServiceWorker';
 import store from './store.js'
 import 'bootstrap/dist/css/bootstrap.css';
-import { configureFakeBackend } from './helpers/fake-backend';
-configureFakeBackend();
 
 ReactDOM.render(
   <Provider store={store} >
