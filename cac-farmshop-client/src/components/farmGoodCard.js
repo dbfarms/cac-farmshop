5b5f9c4b0f71135a978a4bcd6aefa006bbc5bc5b
Matt Hunger <matt.hunger@gmail.com>
2018-02-22 10:16:26 -0500

    Added redirect to farmgoodcard page

--- cac-farmshop-client/src/components/farmGoodCard.js
+++ cac-farmshop-client/src/components/farmGoodCard.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router'
 
 const FarmGoodCard = (props) =>
   
-<div key={props.location.farmGood.farmGood.id} className="FarmGoodsCard" >
+<div className="FarmGoodsCard" >
+{props.location.farmGood === undefined &&
+  <Redirect to="/farm-goods" />
 
+}
+{props.location.farmGood !== undefined &&
+  <div>
   <h4>{props.location.farmGood.farmGood.attributes.name}</h4>
   <img className="farmGoodImage" src={props.location.farmGood.farmGood.attributes.img_url} alt={props.location.farmGood.farmGood.id} />
-  {props.location.farmGood.farmGood.attributes.inventory > 0 &&
+  { props.location.farmGood.farmGood.attributes.inventory > 0 &&
   <p>Available: {props.location.farmGood.farmGood.attributes.inventory} at ${props.location.farmGood.farmGood.attributes.price} each</p>
   }
+
   {props.location.farmGood.farmGood.attributes <= 0 &&
     <p>No longer available. Check back soon</p>
   }
-
+  </div>
+}
 </div>
  
 
