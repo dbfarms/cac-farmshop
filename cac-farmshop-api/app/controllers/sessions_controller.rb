80b87598df63d585f4a73ea8dc911bd083312e07
dbfarms <matt.hunger@gmail.com>
2018-02-28 14:19:28 -0500

    figaro and auth

--- cac-farmshop-api/app/controllers/sessions_controller.rb
+++ cac-farmshop-api/app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     
     user = User.find_by(email: auth_params[:email])
     if user && user.authenticate(auth_params[:password])
+      byebug 
       jwt = Auth.issue({user: user.id})
       render json: {jwt: jwt}
     else
