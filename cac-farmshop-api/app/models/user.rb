1320e406a12dea853c153bc4340535a8255bf812
dbfarms <matt.hunger@gmail.com>
2018-03-11 15:42:32 -0400

    realignd user/farmers with no database scheme

--- cac-farmshop-api/app/models/user.rb
+++ cac-farmshop-api/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_secure_password
+  #has_secure_password
   validates :email, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
-  validates :password, presence: true  #, confirmation: true 
+  #validates :password, presence: true  #, confirmation: true 
   #validates :password_confirmation, presence: true
 
   belongs_to :farmer 
