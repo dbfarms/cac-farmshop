8e0f8ca207b41814b23dacc918469ef32be197a7
dbfarms <matt.hunger@gmail.com>
2018-03-21 11:09:03 -0400

    safety commit

--- cac-farmshop-api/app/controllers/api/carts_controller.rb
+++ cac-farmshop-api/app/controllers/api/carts_controller.rb
@@ -21,7 +21,7 @@ class Api::CartsController < ApplicationController
     end
 
     def update
-        byebug 
+        #byebug 
         if @cart.update(cart_params)
             render json: @cart
         else
@@ -30,11 +30,52 @@ class Api::CartsController < ApplicationController
     end
 
     def destroy
+        #byebug 
+        out_of_stock = []
+        refund = []
+        total = 0
+        #byebug
+        @cart.line_items.each do |li| 
+            #byebug 
+            fg = Farmgood.find(li.farmgood_id)
+            #byebug 
+            if fg.inventory == nil 
+                fg.inventory = 0
+                fg.save 
+                #byebug
+            end 
 
-        byebug 
+            if fg.inventory - li.quantity >= 0
+                fg.inventory -= li.quantity 
+                total = total + (li.quantity * fg.price)
+                fg.save 
+            elsif fg.inventory == 0
+                out_of_stock.push(`#{fg.name} is out of stock`)
+            else 
+                over_order = fg.inventory - li.quantity
+                amount_available = li.quantity + over_order
+                fg.inventory -= amount_available
+                total = total + (amount_available * fg.price)
+                fg.save 
+
+                refund << [fg.name, fg.price]
+            end 
+
+        end 
         
+        @cart.line_items.each {|li| li.delete }
+        user = User.find(@cart.customer_user_id)
+        new_cart = Cart.new 
+        new_cart.user = user 
+        user.cart = new_cart 
+
+        total_refund = 0
+        refund.each {|r| total_refund += r[1]}
+        total -= total_refund
+        byebug
+
         if @cart.destroy
-            render json: { message: "successfully destroyed" }, status: 204
+            render json: { message: "successfully destroyed", errors: out_of_stock, refund: refund, total: total}, status: 204
         else
             render json: { message: "unable to remove this cart" }, status: 400
         end
