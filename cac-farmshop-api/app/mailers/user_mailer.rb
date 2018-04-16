d43befd6fc321938149d37bbc2f647697a685f9e
dbfarms <matt.hunger@gmail.com>
2018-04-16 12:47:52 -0400

    email now works

--- cac-farmshop-api/app/mailers/user_mailer.rb
+++ cac-farmshop-api/app/mailers/user_mailer.rb
@@ -2,9 +2,10 @@ class UserMailer < ApplicationMailer
 
     def order_email(user, order)
         @user = user
-        @order = order 
+        @order = order  
         mail(to: "dirtybootsfarmstore@gmail.com", subject: 'testing')
         #mail(to: @user.email, subject: 'Welcome to My Awesome Site')
     end
 
 end
+
